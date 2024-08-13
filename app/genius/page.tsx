09bc77e6670a92493a219858a66e2c24bb622338
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-08-13 18:17:11 -0300

    chore: improve code structure

--- app/genius/page.tsx
+++ app/genius/page.tsx
@@ -11,6 +11,7 @@ const GeniusGame = () => {
   const [phase, setPhase] = useState(1);
   const [sequenceCount, setSequenceCount] = useState(0);
   const [activeColor, setActiveColor] = useState<string | null>(null);
+  const [clickedColor, setClickedColor] = useState<string | null>(null);
 
   useEffect(() => {
     if (userSequence.length === sequence.length && isUserTurn) {
@@ -67,6 +68,10 @@ const GeniusGame = () => {
   const handleUserClick = (color: string) => {
     if (isUserTurn) {
       setUserSequence([...userSequence, color]);
+      setClickedColor(color);
+      setTimeout(() => {
+        setClickedColor(null);
+      }, 300); // Reset clicked color after 300ms
     }
   };
 
@@ -84,77 +89,59 @@ const GeniusGame = () => {
   const progressPercentage = (sequenceCount / 10) * 100;
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>
+    <div className="text-center">
+      <h1 className="text-2xl font-bold mb-4">
         Mostre que você é craque nos looks C&A Faça a mesma sequência de peças e
         cores para acumular pontos!
       </h1>
       <p>{message}</p>
       <div className="my-10">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "300px",
-          }}
-        >
-          <div
-            style={{
-              display: "grid",
-              gridTemplateRows: "1fr 1fr 1fr",
-              gridTemplateColumns: "1fr 1fr 1fr",
-              gap: "10px",
-            }}
-          >
-            <div style={{ gridRow: "1 / 2", gridColumn: "2 / 3" }}>
+        <div className="flex justify-center items-center h-72">
+          <div className="grid grid-rows-3 grid-cols-3 gap-2">
+            <div className="row-start-1 col-start-2">
               <button
                 onClick={() => handleUserClick("vermelho")}
-                style={{
-                  backgroundColor:
-                    activeColor === "vermelho" ? "lightcoral" : "red",
-                  width: 100,
-                  height: 100,
-                }}
+                className={`w-24 h-24 ${
+                  activeColor === "vermelho" || clickedColor === "vermelho"
+                    ? "bg-red-300"
+                    : "bg-red-600"
+                }`}
               >
                 Vermelho
               </button>
             </div>
-            <div style={{ gridRow: "2 / 3", gridColumn: "1 / 2" }}>
+            <div className="row-start-2 col-start-1">
               <button
                 onClick={() => handleUserClick("azul")}
-                style={{
-                  backgroundColor:
-                    activeColor === "azul" ? "lightblue" : "blue",
-                  width: 100,
-                  height: 100,
-                }}
+                className={`w-24 h-24 ${
+                  activeColor === "azul" || clickedColor === "azul"
+                    ? "bg-blue-300"
+                    : "bg-blue-600"
+                }`}
               >
                 Azul
               </button>
             </div>
-            <div style={{ gridRow: "2 / 3", gridColumn: "3 / 4" }}>
+            <div className="row-start-2 col-start-3">
               <button
                 onClick={() => handleUserClick("amarelo")}
-                style={{
-                  backgroundColor:
-                    activeColor === "amarelo" ? "lightyellow" : "yellow",
-                  width: 100,
-                  height: 100,
-                }}
+                className={`w-24 h-24 ${
+                  activeColor === "amarelo" || clickedColor === "amarelo"
+                    ? "bg-yellow-300"
+                    : "bg-yellow-600"
+                }`}
               >
                 Amarelo
               </button>
             </div>
-            <div style={{ gridRow: "3 / 4", gridColumn: "2 / 3" }}>
+            <div className="row-start-3 col-start-2">
               <button
                 onClick={() => handleUserClick("verde")}
-                style={{
-                  backgroundColor:
-                    activeColor === "verde" ? "lightgreen" : "green",
-                  width: 100,
-                  height: 100,
-                }}
+                className={`w-24 h-24 ${
+                  activeColor === "verde" || clickedColor === "verde"
+                    ? "bg-green-300"
+                    : "bg-green-600"
+                }`}
               >
                 Verde
               </button>
@@ -162,15 +149,20 @@ const GeniusGame = () => {
           </div>
         </div>
       </div>
-      <button onClick={startGame}>Começar</button>
-      <div style={{ marginTop: 20, width: "100%", backgroundColor: "#e0e0e0" }}>
-        <div
-          style={{
-            width: `${progressPercentage}%`,
-            height: 20,
-            backgroundColor: "green",
-          }}
-        ></div>
+      <button
+        className="bg-blue-800 p-4 rounded-xl text-white"
+        onClick={startGame}
+      >
+        Começar
+      </button>
+      <p className="my-5">Sua pontuação</p>
+      <div className="flex items-center justify-center">
+        <div className="w-2/3 bg-gray-200 rounded-full">
+          <div
+            className="bg-green-800 h-4 rounded-full"
+            style={{ width: `${progressPercentage}%` }}
+          ></div>
+        </div>
       </div>
     </div>
   );
