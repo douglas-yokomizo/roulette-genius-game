c19d5030a0bfc60c41604fe9cb640b2db9b0600d
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-09-11 14:15:23 -0300

    refactor: add accessibility mode toggle button

--- app/genius/page.tsx
+++ app/genius/page.tsx
@@ -19,6 +19,7 @@ const GeniusGame = () => {
 
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [countdown, setCountdown] = useState(3);
+  const [isAccessibilityMode, setIsAccessibilityMode] = useState(false);
 
   const getColorClass = (color: string) => {
     if (activeColor === color || clickedColor === color) {
@@ -58,6 +59,10 @@ const GeniusGame = () => {
         startGame();
       }
     }, 1000);
+  };
+
+  const toggleAccessibilityMode = () => {
+    setIsAccessibilityMode(!isAccessibilityMode);
   };
 
   return (
@@ -110,96 +115,191 @@ const GeniusGame = () => {
         )
       )}
       {isGameStarted && countdown === 0 && (
-        <motion.div
-          className="my-10"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <div className="flex justify-center items-center h-72">
-            <div className="grid grid-rows-2 grid-cols-2 gap-10 gap-y-20">
-              <div
-                className="row-start-1 col-start-1 relative"
-                onClick={() => handleUserClick("roxo")}
-              >
-                <Image
-                  src={images.rocamboleRoxo}
-                  alt="Rocambole Roxo C&A"
-                  className={`active-stroke ${getColorClass("roxo")}`}
-                />
-                <Image
-                  src={images.colete}
-                  alt="colete"
-                  className={`absolute inset-0 m-auto w-3/5 object-contain ${getJumpClass(
-                    "roxo"
-                  )}`}
-                />
+        <>
+          <button
+            onClick={toggleAccessibilityMode}
+            className="absolute bottom-5 right-5 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Acessibilidade
+          </button>
+          {isAccessibilityMode ? (
+            <>
+              <div className="flex flex-col items-center transform -translate-y-44 justify-center gap-6">
+                <p className="text-5xl font-bold w-11/12">
+                  acompanhe sua pontuação
+                </p>
+                <div className="w-full bg-transparent border-[6px] border-azul rounded-full mt-4">
+                  <div
+                    className="bg-azul h-20 w-4/5 rounded-full transition-all duration-500 ease-in-out"
+                    style={{ width: `${progressPercentage}%` }}
+                  ></div>
+                </div>
               </div>
-              <div
-                className="row-start-1 col-start-2 relative"
-                onClick={() => handleUserClick("vermelho")}
-              >
-                <Image
-                  src={images.rocamboleVermelho}
-                  alt="Rocambole Vermelho C&A"
-                  className={`active-stroke ${getColorClass("vermelho")}`}
-                />
-                <Image
-                  src={images.camiseta}
-                  alt="camiseta"
-                  className={`absolute inset-0 m-auto w-5/6 object-contain ${getJumpClass(
-                    "vermelho"
-                  )}`}
-                />
+              <motion.div className="mt-8 mb-2">
+                <div className="flex justify-center items-center">
+                  <div className="grid grid-rows-2 grid-cols-2 gap-10 gap-y-20">
+                    <div
+                      className="row-start-1 col-start-1 relative"
+                      onClick={() => handleUserClick("roxo")}
+                    >
+                      <Image
+                        src={images.rocamboleRoxo}
+                        alt="Rocambole Roxo C&A"
+                        className={`active-stroke ${getColorClass("roxo")}`}
+                      />
+                      <Image
+                        src={images.colete}
+                        alt="colete"
+                        className={`absolute inset-0 m-auto w-3/5 object-contain ${getJumpClass(
+                          "roxo"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-1 col-start-2 relative"
+                      onClick={() => handleUserClick("vermelho")}
+                    >
+                      <Image
+                        src={images.rocamboleVermelho}
+                        alt="Rocambole Vermelho C&A"
+                        className={`active-stroke ${getColorClass("vermelho")}`}
+                      />
+                      <Image
+                        src={images.camiseta}
+                        alt="camiseta"
+                        className={`absolute inset-0 m-auto w-5/6 object-contain ${getJumpClass(
+                          "vermelho"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-2 col-start-1 relative"
+                      onClick={() => handleUserClick("rosa")}
+                    >
+                      <Image
+                        src={images.rocamboleRosa}
+                        alt="Rocambole Rosa C&A"
+                        className={`active-stroke ${getColorClass("rosa")}`}
+                      />
+                      <Image
+                        src={images.shorts}
+                        alt="shorts"
+                        className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
+                          "rosa"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-2 col-start-2 relative"
+                      onClick={() => handleUserClick("amarelo")}
+                    >
+                      <Image
+                        src={images.rocamboleAmarelo}
+                        alt="Rocambole Amarelo C&A"
+                        className={`active-stroke ${getColorClass("amarelo")}`}
+                      />
+                      <Image
+                        src={images.bermuda}
+                        alt="bermuda"
+                        className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
+                          "amarelo"
+                        )}`}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            </>
+          ) : (
+            <>
+              <motion.div className="my-10">
+                <div className="flex justify-center items-center h-72">
+                  <div className="grid grid-rows-2 grid-cols-2 gap-10 gap-y-20">
+                    <div
+                      className="row-start-1 col-start-1 relative"
+                      onClick={() => handleUserClick("roxo")}
+                    >
+                      <Image
+                        src={images.rocamboleRoxo}
+                        alt="Rocambole Roxo C&A"
+                        className={`active-stroke ${getColorClass("roxo")}`}
+                      />
+                      <Image
+                        src={images.colete}
+                        alt="colete"
+                        className={`absolute inset-0 m-auto w-3/5 object-contain ${getJumpClass(
+                          "roxo"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-1 col-start-2 relative"
+                      onClick={() => handleUserClick("vermelho")}
+                    >
+                      <Image
+                        src={images.rocamboleVermelho}
+                        alt="Rocambole Vermelho C&A"
+                        className={`active-stroke ${getColorClass("vermelho")}`}
+                      />
+                      <Image
+                        src={images.camiseta}
+                        alt="camiseta"
+                        className={`absolute inset-0 m-auto w-5/6 object-contain ${getJumpClass(
+                          "vermelho"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-2 col-start-1 relative"
+                      onClick={() => handleUserClick("rosa")}
+                    >
+                      <Image
+                        src={images.rocamboleRosa}
+                        alt="Rocambole Rosa C&A"
+                        className={`active-stroke ${getColorClass("rosa")}`}
+                      />
+                      <Image
+                        src={images.shorts}
+                        alt="shorts"
+                        className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
+                          "rosa"
+                        )}`}
+                      />
+                    </div>
+                    <div
+                      className="row-start-2 col-start-2 relative"
+                      onClick={() => handleUserClick("amarelo")}
+                    >
+                      <Image
+                        src={images.rocamboleAmarelo}
+                        alt="Rocambole Amarelo C&A"
+                        className={`active-stroke ${getColorClass("amarelo")}`}
+                      />
+                      <Image
+                        src={images.bermuda}
+                        alt="bermuda"
+                        className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
+                          "amarelo"
+                        )}`}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+              <div className="flex flex-col items-center justify-center mt-64 gap-6">
+                <p className="text-5xl font-bold w-11/12">
+                  acompanhe sua pontuação
+                </p>
+                <div className="w-full bg-transparent border-[6px] border-azul rounded-full mt-4">
+                  <div
+                    className="bg-azul h-20 w-4/5 rounded-full transition-all duration-500 ease-in-out"
+                    style={{ width: `${progressPercentage}%` }}
+                  ></div>
+                </div>
               </div>
-              <div
-                className="row-start-2 col-start-1 relative"
-                onClick={() => handleUserClick("rosa")}
-              >
-                <Image
-                  src={images.rocamboleRosa}
-                  alt="Rocambole Rosa C&A"
-                  className={`active-stroke ${getColorClass("rosa")}`}
-                />
-                <Image
-                  src={images.shorts}
-                  alt="shorts"
-                  className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
-                    "rosa"
-                  )}`}
-                />
-              </div>
-              <div
-                className="row-start-2 col-start-2 relative"
-                onClick={() => handleUserClick("amarelo")}
-              >
-                <Image
-                  src={images.rocamboleAmarelo}
-                  alt="Rocambole Amarelo C&A"
-                  className={`active-stroke ${getColorClass("amarelo")}`}
-                />
-                <Image
-                  src={images.bermuda}
-                  alt="bermuda"
-                  className={`absolute inset-0 m-auto w-[90%] object-contain ${getJumpClass(
-                    "amarelo"
-                  )}`}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center mt-64 gap-6">
-            <p className="text-5xl font-bold w-11/12">
-              acompanhe sua pontuação
-            </p>
-            <div className="w-full bg-transparent border-[6px] border-azul rounded-full mt-4">
-              <div
-                className="bg-azul h-20 w-4/5 rounded-full transition-all duration-500 ease-in-out"
-                style={{ width: `${progressPercentage}%` }}
-              ></div>
-            </div>
-          </div>
-        </motion.div>
+            </>
+          )}
+        </>
       )}
       {message && <p className=" text-4xl my-5">{message}</p>}
       <Image
